3e36fdc0997225112d588a2211a0c3d9d3dac569
Dj-777 <dchampavat777@gmail.com>
2023-10-06 16:00:52 +0530

    Bug fixes in conform password

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import './LoginSignup.css'
-import userIcon from '../Assets/person.png'
-import userEmail from  '../Assets/email.png'
-import userPassword from "../Assets/password.png"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./LoginSignup.css";
+import userIcon from "../Assets/person.png";
+import userEmail from "../Assets/email.png";
+import userPassword from "../Assets/password.png";
 const LoginSignup = () => {
   const [action, setAction] = useState("Sign Up");
   const [name, setName] = useState(null);
@@ -46,8 +46,8 @@ const LoginSignup = () => {
       setConfirmPassword(value);
     }
   };
- 
-  const  handleSubmitChange = (name, email, password,confirmPassword) => {
+
+  const handleSubmitChange = (name, email, password, confirmPassword) => {
     console.log(name, email, password, "e");
     // let getUserData = JSON.parse(localStorage.getItem("User"));
     // console.log(getUserData);
@@ -60,47 +60,44 @@ const LoginSignup = () => {
     //       );
     //     } else {
     //       let id = Math.floor(Math.random() * 4000);
-          // let userData = [
-          //   ...getUserData,
-          //   { name: name, email: email, password: password, id: id },
-          // ];
-          // localStorage.setItem("User", JSON.stringify(userData));
-        
-           axios.post("http://localhost:3000/user/users",{
-            data: ({name,email,password,confirmPassword}),
-          }).then((res)=>{
-            console.log(res.data.message,"res");
-           if( res.data.status === 1 ){
-             setSuccessMessage(
-               res.data.message
-             )
-             setTimeout(() => {
-               setAction("Login");
-              setErrMessage(null);
-              setSuccessMessage(null);
-            }, 2000);
-           } 
-            else if(res.data.status === 0) {
-              setErrMessage(res.data.message) 
-              setTimeout(() => {
-                // setAction("Login");
-               setErrMessage(null);
-               setSuccessMessage(null);
-             }, 2000);
-            } 
-          }).catch((error)=>{
-            console.log(error);
-            setErrMessage(
-              error.data.message
-            );
-          })
-         
+    // let userData = [
+    //   ...getUserData,
+    //   { name: name, email: email, password: password, id: id },
+    // ];
+    // localStorage.setItem("User", JSON.stringify(userData));
+
+    axios
+      .post("http://localhost:3000/user/users", {
+        data: { name, email, password, confirmPassword },
+      })
+      .then((res) => {
+        console.log(res?.data?.message, "res");
+        if (res?.data?.status === 1) {
+          setSuccessMessage(res?.data?.message);
+          setTimeout(() => {
+            setAction("Login");
+            setErrMessage(null);
+            setSuccessMessage(null);
+          }, 2000);
+        } else if (res?.data?.status === 0) {
+          setErrMessage(res?.data?.message);
+          setTimeout(() => {
+            // setAction("Login");
+            setErrMessage(null);
+            setSuccessMessage(null);
+          }, 2000);
         }
-      // });
-    // } else {
-      // let id = Math.floor(Math.random() * 4000);
-      // let userData = [{ name: name, email: email, password: password, id: id }];
-      // localStorage.setItem("User", JSON.stringify(userData));
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrMessage(error?.data?.message);
+      });
+  };
+  // });
+  // } else {
+  // let id = Math.floor(Math.random() * 4000);
+  // let userData = [{ name: name, email: email, password: password, id: id }];
+  // localStorage.setItem("User", JSON.stringify(userData));
   //     setSuccessMessage("Congratulations, you have successfully registered");
   //   }
   // };
@@ -141,43 +138,52 @@ const LoginSignup = () => {
           <div></div>
         ) : (
           <div>
-            <div className='input'>
-            
-            <img src={userIcon} alt="" />
-            <input
-              type="text"
-              placeholder="Enter Name"
-              onChange={(e) => handelInputForUser(e)}
-              id="name"
-              value={name}
-              
-            />
-           </div>
-          
-          {/* <div className='inputConfromPAss'>
-
-                <img src={userPassword} alt="" />
-                <input type="password" 
-                placeholder='Enter conform-Password' 
-                onChange={(data)=>handelInputForUser(data)} value={confirmPassword} 
-                id="conformpassword" />
-          </div> */}
-
+            <div className="input">
+              <img src={userIcon} alt="" />
+              <input
+                type="text"
+                placeholder="Enter Name"
+                onChange={(e) => handelInputForUser(e)}
+                id="name"
+                value={name}
+              />
+            </div>
+            {/* <div className="inputConfromPAss">
+              <img src={userPassword} alt="" />
+              <input
+                type="password"
+                placeholder="Enter conform-Password"
+                onChange={(data) => handelInputForUser(data)}
+                value={confirmPassword}
+                id="conformpassword"
+              />
+            </div> */}
           </div>
         )}
-           <div className="input">
-                <img src={userEmail} alt="" />
-                <input type="email" placeholder='Enter Email Id' onChange={(data)=>handelInputForUser(data)} value={email} id="email" />
-            </div>
-            <div className="input">
-                <img src={userPassword} alt="" />
-                <input type="password" placeholder='Enter Password' onChange={(data)=>handelInputForUser(data)} value={password} id="password" />
-            </div>
-            
-            {/* {error === null ? <div></div> : <span className='passwordError'>{error}</span>} */}
-        
-     
-        {/* {action === "Sign Up" && (
+        <div className="input">
+          <img src={userEmail} alt="" />
+          <input
+            type="email"
+            placeholder="Enter Email Id"
+            onChange={(data) => handelInputForUser(data)}
+            value={email}
+            id="email"
+          />
+        </div>
+        <div className="input">
+          <img src={userPassword} alt="" />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(data) => handelInputForUser(data)}
+            value={password}
+            id="password"
+          />
+        </div>
+
+        {/* {error === null ? <div></div> : <span className='passwordError'>{error}</span>} */}
+
+        {action === "Sign Up" && (
           <div className="input">
             <img src={userPassword} alt="" />
             <input
@@ -188,46 +194,42 @@ const LoginSignup = () => {
               value={confirmPassword}
             />
           </div>
-        )}  */}
-    
-      {action === "Sign Up" ? (
-       <div></div>
-      ) : (
-        <div className="forgot-password">
-          Lost Password? <span>Click Here!</span>
-        </div>
-      )}
-      <span className="errMessage">{errMessage}</span>
-      {successMessage !== null && (
-        <span className="successMessage">{successMessage}</span>
-      )}
-      <div className="submit-container">
-        <div
-          className={action === "Login" ? "submit gray" : "submit"}
-          onClick={() => {
-            setAction("Sign Up");
-            handleSubmitChange(name, email, password,confirmPassword);
-          }}
-        >
-          Sign Up
-        </div>
-        <div
-          className={action === "Sign Up" ? "submit gray" : "submit"}
-          
-          // onClick={() => {
-          //   setAction("Login");
-          //   handleLoginChange(email, password);
-          // }}
-        >
-          Login
+        )}
+
+        {action === "Sign Up" ? (
+          <div></div>
+        ) : (
+          <div className="forgot-password">
+            Lost Password? <span>Click Here!</span>
+          </div>
+        )}
+        <span className="errMessage">{errMessage}</span>
+        {successMessage !== null && (
+          <span className="successMessage">{successMessage}</span>
+        )}
+        <div className="submit-container">
+          <div
+            className={action === "Login" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Sign Up");
+              handleSubmitChange(name, email, password, confirmPassword);
+            }}
+          >
+            Sign Up
+          </div>
+          <div
+            className={action === "Sign Up" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Login");
+              // handleLoginChange(email, password);
+            }}
+          >
+            Login
+          </div>
         </div>
       </div>
     </div>
-    </div>
-
-    
-
-  )
-}
+  );
+};
 
 export default LoginSignup;
